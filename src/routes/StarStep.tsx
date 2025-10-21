82ec75ea39177cd39e30ffe0101194c054d8a3e0
Michael L Petersen <ekmlpe@danskespil.dk>
2025-10-21 13:01:15 +0200

    add images to starstep

--- src/routes/StarStep.tsx
+++ src/routes/StarStep.tsx
@@ -10,6 +10,12 @@ function RouteComponent() {
   const props = {
     title: 'Optjente lodder i alt',
     text: 'Ugens lodtrækning finder sted hver søndag kl. 24:00. Jo flere lodder, desto større vinderchance.',
+    lotGained: {
+      src: '/Components/DanskeSpil/Domain/Feature.KlubLotto/Graphics/StarStep/star-full.svg',
+    },
+    lotNotGained: {
+      src: '/Components/DanskeSpil/Domain/Feature.KlubLotto/Graphics/StarStep/star-empty.svg',
+    },
   };
   return <StarStep {...props} />;
 }
